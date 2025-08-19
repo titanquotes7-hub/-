e018b74a3c3d2f0f6639677ae82163d059dc3d96
titanquotes7-hub <titanquotes7@gmail.com>
2025-08-19 23:51:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;SBs1yJyT#3fqCDfdEDJQhuKizNC6LME51TahWDGMFfNInA0KTczQ", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
